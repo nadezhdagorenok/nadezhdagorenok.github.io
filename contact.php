b5b34465c796e7ea83eef234097339ffde4295e4
Nadezhda Gorenok <n.gorenok@gmail.com>
2017-06-16 05:47:17 +0000

    twenty

--- contact.php
+++ contact.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1"> 
+        <title>Contact</title>
+        
+    </head>
+    <body>
 <?php
 
 $otvet=mail("nadin_19_@mail.ru", "Тема письма", "Сообщение письма - name: $_POST[name] Email: $_POST[email] ");
@@ -8,3 +17,5 @@ if($otvet){
 else echo "<p>Письмо не было отправлено</p>";
 
 ?>
+</body>
+</html>
